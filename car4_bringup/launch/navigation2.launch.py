3bfa0ddf9362229a2b27b493cda0137b08729136
KsaweryAiR <85024534+KsaweryAiR@users.noreply.github.com>
2024-06-07 01:37:23 +0200

    fiz nav

---
+++ car4_bringup/launch/navigation2.launch.py
@@ -0,0 +1,107 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_path = LaunchConfiguration('urdf_path')
+    world_path = LaunchConfiguration('world_path')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('car4_bringup'),
+            'map',
+            'level2.yaml'))
+    param_file_name = 'car4_param.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('car4_bringup'),
+            'param',
+            param_file_name))
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'urdf_path',
+            default_value=os.path.join(
+                get_package_share_directory('car4_description'),
+                'urdf',
+                'my_robot.urdf.xacro'),
+            description='Path to the URDF file'
+        ),
+        DeclareLaunchArgument(
+            'world_path',
+            default_value=os.path.join(
+                get_package_share_directory('car4_bringup'),
+                'worlds',
+                'level2.world'),
+            description='Path to the Gazebo world file'
+        ),
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'
+        ),
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'
+        ),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', urdf_path])}]
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                os.path.join(
+                    get_package_share_directory('gazebo_ros'),
+                    'launch',
+                    'gazebo.launch.py'
+                )
+            ]),
+            launch_arguments={'world': world_path}.items()
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_entity',
+            output='screen',
+            arguments=['-topic', 'robot_description', '-entity', 'my_robot']
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+    ])
